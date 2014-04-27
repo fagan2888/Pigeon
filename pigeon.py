b064a5c7cb176ffd807be2e04f08241074039fe8
Shi Yang <kid7st@gmail.com>
2014-04-27 12:00:03 +0800

    available version 1.0

--- pigeon.py
+++ pigeon.py
@@ -5,6 +5,7 @@ import urllib2
 import urllib
 import csv
 import sys
+import json
 
 AK = "OZPNC51CMPEyD65Qzk2L2x5y"
 
@@ -17,33 +18,44 @@ def get_distance(points, tactics = 12, ak = AK, mode='driving', coord_type = 'wg
         coord_type: 坐标类型，默认wgs84;
         tactics: 导航策略，10表示不走高速，11表示最少时间，12表示最短路径，默认最短路径;
         ak: 百度地图访问密钥；
+    Returns:
+        distance: 返回路径距离，单位: m
+        duration: 返回路径时间，单位: s
     '''
     api_url = "http://api.map.baidu.com/direction/v1/routematrix"
+    distance = 0
+    duration = 0
     for i in range(len(points)-1):
         origin_str = str(points[i]['n']) + ',' + str(points[i]['e'])
         dest_str = str(points[i+1]['n']) + ',' + str(points[i+1]['e'])
+        # 传递给API的查询参数
         values = {
-                'output':       'json',
-                'tactics':      str(tactics),
-                'mode':         'driving',
-                'coord_type':   coord_type,
-                'ak':           ak,
-                'origins':      origin_str,
-                'destinations': dest_str
+                'output':       'json',             #查询返回数据格式
+                'tactics':      str(tactics),       #导航策略
+                'mode':         'driving',          #导航模式
+                'coord_type':   coord_type,         #坐标类型
+                'ak':           ak,                 #账号密钥
+                'origins':      origin_str,         #起点坐标串
+                'destinations': dest_str            #终点坐标串
                 }
 
         data = urllib.urlencode(values)
         url = api_url + '?' + data
-        print url
+        #print url
         response = urllib2.urlopen(url)
-        res = response.read()
-        print res 
-        
-    return 10, 21
+        json_res = response.read()
+        res = json.loads(json_res)
+        #print "status: ", res['status'], "message: ", res['message']
+        distance += res['result']['elements'][0]['distance']['value']
+        duration += res['result']['elements'][0]['duration']['value']
+
+    #print distance, duration
+    return distance, duration
 
 if __name__ == "__main__":
-    reader = csv.reader(open("./data/test.csv"))
-    writer = csv.writer(sys.stdout)
+    reader = csv.reader(open(sys.argv[1]))
+    writer = csv.writer(open(sys.argv[2], 'w'))
+    writer_std = csv.writer(sys.stdout)
 
     cnt = -1 
     points = []
@@ -64,7 +76,14 @@ if __name__ == "__main__":
                         best_time_distance, best_time_duration,         
                         best_distance_distance, best_distance_duration, 
                         no_highspeed_distance, no_highspeed_duration))
-    
+
+                #输出log
+                writer_std.writerow((current_car_id, start_date, start_time, cnt,  
+                        best_time_distance, best_time_duration,         
+                        best_distance_distance, best_distance_duration, 
+                        no_highspeed_distance, no_highspeed_duration))
+
+   
             # 开始计算新的路径
             current_car_id = car_id
             start_date = gps_date
@@ -84,4 +103,12 @@ if __name__ == "__main__":
             best_time_distance, best_time_duration,         
             best_distance_distance, best_distance_duration, 
             no_highspeed_distance, no_highspeed_duration))
+
+    #输出log
+    writer_std = csv.writer(sys.stdout)
+    writer_std.writerow((current_car_id, start_date, start_time, cnt,  
+            best_time_distance, best_time_duration,         
+            best_distance_distance, best_distance_duration, 
+            no_highspeed_distance, no_highspeed_duration))
+
 
